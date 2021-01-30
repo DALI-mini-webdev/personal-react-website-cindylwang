51ea673046df70f0babf6bf3b1da54011bde395c
cindylwang <cindy.l.wang.24@dartmouth.edu>
2021-01-30 04:06:39 -0500

    added button and edited text

--- src/App.js
+++ src/App.js
@@ -6,33 +6,26 @@ function App() {
   <h1 className="darkpurple-text"> My name is {name}!</h1> : <p> My name is not Cindy.</p>
   const buttonFunction = () =>
     console.log("The button was clicked.");
+    <button onClick="window.location='https://www.youtube.com/watch?v=rEq1Z0bjdwc';"></button>
   
   return (
-    <div className = "App-header">
-      <head>
-        <title>Cindy Wang</title> 
-      </head>
-
+    <div className="App">
       <header className = "App-header">
         <div>{nameComponent}</div>
         <button onClick={buttonFunction}>Click me!</button>
       </header>
 
-      <body className="App">
+      <body className="App-header">
           <p className="lilac-text">I am from Fargo, North Dakota.</p>
-          <img src="Fargo.jpg" 
-          widgth="500" height="250"></img>
+          <img src="Fargo.jpg" widgth="500" height="250" alt="Fargo"></img>
 
           <p className="lilac-text">This term, I'll be living with a few friends in NYC!</p>
-          <img src="NYC.jpg"
-          width="445" height="250"></img>
+          <img src="NYC.jpg" width="445" height="250" alt="NYC"></img>
       </body>
-
-      <p className="darkpurple-text">--------------------------------------------------------------------------------------------</p>
-      
+        
       <div className= "App-header">
           <p className="lilac-text">I am planning on majoring in computer science and economics.</p>
-  
+    
           <p className="lilac-text">The classes I'm taking for this quarter are: </p>
               <ul className="brown-text">
                   <li>ECON 1</li>
@@ -41,14 +34,14 @@ function App() {
                   <li>WGSS 15</li>
               </ul>
 
-          <p className="App" className="lilac-text">Some clubs that I've joined are:</p>
+          <p className="lilac-text">Some clubs that I've joined are:</p>
               <ul className="brown-text">
                   <li>Formula Racing</li>
                   <li>Dartmouth Outing Club (DOC)</li>
                   <li>Women in Computer Science (WiCS)</li>
                   <li>Dartmouth Undergraduate Journal of Science (DUJS)</li>
               </ul>
-          
+            
           <p className="lilac-text">In my free time, some things that I like to do are:</p>
               <ul className="brown-text">
                   <li>Go for a run or hike</li>
@@ -60,8 +53,8 @@ function App() {
                   <li>Paint</li>
               </ul>
 
-          <p className="lilac-text">I am excited to be part of this course and I am looking forward 
-              to learning more about web development!</p>
+          <p className="lilac-text">I am excited to be part of this course and I am looking 
+          forward to learning more about web development!</p>
     
     </div>
     </div>
